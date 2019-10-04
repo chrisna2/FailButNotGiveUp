6e949bd87f1fcb57c2e9a36b9871388e9348877f
chrisna2 <chrisna2@hanmail,net>
2019-10-04 10:52:47 +0900

    일단 목업데이터는 지우고 , 문제4번 테스트 추가

--- src/test/java/assignment/kakaopay/pickme/function4/function4Test.java
+++ src/test/java/assignment/kakaopay/pickme/function4/function4Test.java
@@ -1,12 +1,11 @@
 package assignment.kakaopay.pickme.function4;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +16,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import assignment.kakaopay.pickme.controller.KakaoController;
 import assignment.kakaopay.pickme.service.KakaoService;
@@ -36,10 +37,19 @@ public class function4Test {
 	@MockBean
 	private KakaoService service;
 	
+	ObjectMapper mapper = new ObjectMapper();
+	
 	@Test
 	public void function4() throws Exception{
+		
+		HashMap<String, String> testParam = new HashMap<>();
+		testParam.put("brName", "판교점");
+		
+		
 		//KakaoController의  "/function4" 매핑으로 정의
-		mockMvc.perform(post("/function4"))
+		//입력값 설정
+		mockMvc.perform(post("/function4").contentType(MediaType.APPLICATION_JSON_UTF8)
+										  .content(mapper.writeValueAsString(testParam)))
 			    //처리내용 출력
 			   .andDo(print())
 			    //테스트 상태의 값은 200 OK (정상)
